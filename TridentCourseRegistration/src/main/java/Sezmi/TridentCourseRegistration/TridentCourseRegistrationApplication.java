4a6dc43524e8dedccd7635fe832168c78d206271
Jeremy Hunton <jhunton22@gmail.com>
2021-09-21 09:50:38 -0400

    Spring Initilizr
    Created a Spring Initilizr for Spring Boot. Using Spring Web, Thymeleaf, Spring Data JPA, and MySQL Driver as the dependencies

---
+++ TridentCourseRegistration/src/main/java/Sezmi/TridentCourseRegistration/TridentCourseRegistrationApplication.java
@@ -0,0 +1,13 @@
+package Sezmi.TridentCourseRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TridentCourseRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TridentCourseRegistrationApplication.class, args);
+	}
+
+}
---
+++ TridentCourseRegistration/src/test/java/Sezmi/TridentCourseRegistration/TridentCourseRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package Sezmi.TridentCourseRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TridentCourseRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
