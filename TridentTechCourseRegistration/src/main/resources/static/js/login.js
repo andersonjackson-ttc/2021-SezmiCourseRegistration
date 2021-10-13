eccc929ea6a5fd98c365a969600b845ace18ade0
brstockton48 <90619690+brstockton48@users.noreply.github.com>
2021-10-12 21:24:26 -0400

    Update Login files
    Updated login.html and login.js files to check the username against static value

--- TridentTechCourseRegistration/src/main/resources/static/js/login.js
+++ TridentTechCourseRegistration/src/main/resources/static/js/login.js
@@ -4,7 +4,25 @@ var email;
 var password;
 
 //Activate Event Listeners on Page Load
-window.onload = function()
+window.onload = function() 
+    {
+    document.getElementById("btn-log-on").addEventListener("click", change, false);
+    }
+    function change() {
+        
+        email = document.getElementById("username-text-box").value;
+        password = document.getElementById("password-text-box").value;
+        
+        if (email=="sezmi123@aol.com" & password=="goteam")
+        window.location = "file://localhost/C:/Users/18436/Documents/GitHub/SezmiCourseRegistration/TridentTechCourseRegistration/src/main/resources/static/index.html";
+        else
+        alert("Username Not Found in Database, please click new user to set up account");
+       
+       // btn-log-on.disabled = true
+
+    }
+
+/*window.onload = function()
 {
     init();
 };
@@ -12,6 +30,52 @@ window.onload = function()
 //Initialize all Event Listeners
 function init()
 {
-    loadMajors();
-    document.getElementById('btn-log-on').addEventListener('click', , false);
+   
+    let button = document.querySelector(".btn-log-on")
+     button.disabled = true
+    //document.getElementsByClassName('btn-log-on').addEventListener('click', checkUser, false);
+    //document.getElementsByClassName('btn-log-on').onclick = 
+    //let button = document.getElementsByClassName('btn-log-on');
+   // button.disabled=true;
+
 }
+
+function redirectToURL(btnId){
+    if(btnId=="btn-log-off")
+        button.disabled = true
+        else
+        window.location.replace("https://en.wikipedia.org/wiki/Main_Page");
+    
+}
+function checkUser()
+{
+    
+    //('btn-log-on').disabled
+    //email = document.getElementById("username-text-box").value;
+    //password = document.getElementById("password-text-box").value;
+    //if (email="sezmi123@aol.com")
+    //{
+     //   btn-log-on.disabled = true; //setting button state to disabled
+
+   //let input = document.querySelector("btn-log-on");
+   //et button = document.querySelector(".btn-log-on");
+   
+   //button.disabled = true; //setting button state to disabled
+   
+       
+    //}
+
+}
+
+//indow.navigate("index.html");
+//Template Literal to Return email and password
+//function emailTemplate(email) 
+//{
+//    return `<option value='${email.email}'>${email.password}</option>`
+//}
+
+//Create API Call
+//xmlhttp.open("GET", '/student');
+            
+//Send API Call
+//xmlhttp.send()*/
