ec55bdcb5045086df1b0a1eb123664eb2e7a8bc2
Jeremy Hunton <jhunton22@gmail.com>
2021-11-18 10:53:50 -0500

    Added Method to return StudentEmailMajors in a list
    added method to return StudentEmailMajors in a list to replace our get mapping for ALL students. After further investigation, we will need to implement this in the next sprint.

--- TridentTechCourseRegistration/src/main/java/Sezmi/TridentTechCourseRegistration/student/StudentRepository.java
+++ TridentTechCourseRegistration/src/main/java/Sezmi/TridentTechCourseRegistration/student/StudentRepository.java
@@ -1,5 +1,7 @@
 package Sezmi.TridentTechCourseRegistration.student;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,4 +18,9 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 	//this Query will return a student's email and major given it's email address
 	@Query(value = "SELECT email, major_id FROM student s WHERE s.email = ?1", nativeQuery = true)
 	StudentEmailMajor getStudentEmailMajors(String email);
+	
+	
+	//this Query returns the student's email and major given for ALL students (to be implemented in another sprint
+	@Query(value = "SELECT email, major_id FROM student", nativeQuery = true)
+	List<StudentEmailMajor> getAllStudentEmailMajor();
 }
